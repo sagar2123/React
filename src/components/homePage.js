b9bb11b4d00551d0e302e77a6b8b1ebaaea34fa3
sagar\sagar_2621 <sagarb@triconinfotech.com>
2018-02-19 10:40:32 +0530

    initial commit

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function(){
+        return (
+          <div className="jumbotron">
+            <h1>Sagar BHatia</h1>
+            <p>React,React router</p>
+          </div>
+        );
+    }
+});
+
+module.exports = Home;
