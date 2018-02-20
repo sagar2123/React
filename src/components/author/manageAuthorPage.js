40b7145be6feb16291d9979e42c1f82a9201383c
sagar\sagar_2621 <sagarb@triconinfotech.com>
2018-02-20 09:24:47 +0530

    saving form

--- src/components/author/manageAuthorPage.js
+++ src/components/author/manageAuthorPage.js
@@ -1,11 +1,40 @@
 "use strict"
 
 var React = require('react');
+var Router = require('react-router');
+var AuhorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var manageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+    getInitialState: function(){
+        return({
+            author: {id: '', firstName: '', lstName: ''}
+        }
+        );
+    },
+
+    setAuthorState: function(event){
+        var field = event.target.name;
+        var value = event.target.value;
+        this.state.author[field] = value;
+        return this.setState({author: this.state.author})
+    },
+
+    saveAuthor: function(event){
+        event.preventDefault();
+        AuthorApi.saveAuthor(this.state.author);
+        this.transitionTo('authors');
+    },
+
     render: function(){
         return(
-            <h1>Manage Author</h1>    
+            <div>
+                <h1>Manage Author</h1>  
+                <AuhorForm author= {this.state.author} onChange={this.setAuthorState} onSave = {this.saveAuthor}/>
+            </div>  
         );
     }
 })
